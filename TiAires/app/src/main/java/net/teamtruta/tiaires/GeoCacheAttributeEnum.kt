886903841d0c9fe82f8bd842ae986b95eac99129
MargaridaMC <margaridamcosta.96@gmail.com>
2020-12-31 01:26:12 +0000

    started converting classes to kotlin classes

---
+++ TiAires/app/src/main/java/net/teamtruta/tiaires/GeoCacheAttributeEnum.kt
@@ -0,0 +1,18 @@
+package net.teamtruta.tiaires
+
+enum class GeoCacheAttributeEnum(var attributeString: String) {
+    BoatRequired("Boat required"), ClimbingGearRequired("Climbing gear required"), DangerousArea("Dangerous area"), FlashlightRequired("Flashlight required"), MayRequireSnowShoes("May require snowshoes"), MayRequireSkis("May require cross country skis"), MayRequireSwimming("May require swimming"), MayRequireWading("May require wading"), NightCache("Night cache"), NotAvailable47("Not available 24/7"), ScubaGearRequired("Scuba gear required"), SeasonalAccessOnly("Seasonal access only"), SpecialToolRequired("Special tool required"), TeamworkCache("Teamwork cache"), TreeClimbingRequired("Tree climbing required"), UVLightRequired("UV light required"), NeedsMaintenance("Needs maintenance"),  //"Field puzzle"
+    None("None");
+
+    companion object {
+        fun valueOfString(typeString: String): GeoCacheAttributeEnum {
+            for (e in values()) {
+                if (e.attributeString == typeString) {
+                    return e
+                }
+            }
+            return None
+        }
+    }
+
+}
---
+++ TiAires/app/src/main/java/net/teamtruta/tiaires/VisitOutcomeEnum.kt
@@ -0,0 +1,15 @@
+package net.teamtruta.tiaires
+
+enum class VisitOutcomeEnum(var visitOutcomeString: String) {
+    NotAttempted(""), DNF("Didn't find it"), Found("Found it"), Note("Write note"), Disabled("Temporarily Disable Listing"), NeedsMaintenance("Needs Maintenance");
+
+    companion object {
+        fun valueOfString(foundTypeString: String): VisitOutcomeEnum {
+            for (e in values()) {
+                if (e.visitOutcomeString == foundTypeString) return e
+            }
+            return NotAttempted
+        }
+    }
+
+}
